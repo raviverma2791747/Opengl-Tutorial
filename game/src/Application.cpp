cbca78ca77edc51d4755a7e611dbd369bfa9d2b5
ravi verma <ravivermaj488@gmail.com>
2020-05-25 09:00:17 +0530

    Rendering Multiple Objects

--- game/src/Application.cpp
+++ game/src/Application.cpp
@@ -2,6 +2,7 @@
 #include<GL/glew.h>
 #include <glfw3.h>
 #include<iostream>
+
 #include"Renderer.h"
 #include"VertexBuffer.h"
 #include"VertexBufferLayout.h"
@@ -9,6 +10,7 @@
 #include"VertexArray.h"
 #include"Shader.h"
 #include"Texture.h"
+
 #include"vendor/glm/glm/glm.hpp"
 #include"vendor/glm/glm/gtc/matrix_transform.hpp"
 
@@ -49,10 +51,10 @@ int main(void)
     {
         float positions[] =
         {   
-            100.0f, 100.0f, 0.0f, 0.0f, // 0
-            200.0f, 100.0f, 1.0f, 0.0f, // 1
-            200.0f, 200.0f, 1.0f, 1.0f, // 2
-            100.0f, 200.0f, 0.0f, 1.0f  // 3
+           -50.0f, -50.0f, 0.0f, 0.0f, // 0
+            50.0f, -50.0f, 1.0f, 0.0f, // 1
+            50.0f,  50.0f, 1.0f, 1.0f, // 2
+           -50.0f,  50.0f, 0.0f, 1.0f  // 3
         };   
        
         unsigned int indices[] = {
@@ -68,8 +70,7 @@ int main(void)
         IndexBuffer ib(indices, 6);
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
        
 
         VertexBufferLayout layout;
@@ -93,8 +94,9 @@ int main(void)
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        glm::vec3 translation(200, 200, 0);
-        
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
+
         /*Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
@@ -102,18 +104,28 @@ int main(void)
             renderer.Clear();
 
             ImGui_ImplGlfwGL3_NewFrame();
-            glm::mat4 model = glm::translate(glm::mat4(1.0f),translation );
+           
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-            glm::mat4 mvp = proj * view * model;
+                renderer.Draw(va, ib, shader);
+            }
 
-            shader.Bind();
-            shader.SetUniforms4f("u_Color", 1.0f, 0.0f, 0.0f,1.0f);
-            shader.SetUniformMat4f("u_MVP", mvp);
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-            renderer.Draw(va, ib, shader);
+                renderer.Draw(va, ib, shader);
+            }
 
             {             
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);               
+                ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);       
+                ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             }
 
